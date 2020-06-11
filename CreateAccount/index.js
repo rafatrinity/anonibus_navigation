50b564b4f287b71e6e59103c4a31ed7c818ada3d
Thamiresvs <thamiresvasconcelos23@gmail.com>
2020-06-11 18:45:24 -0300

    Alterações nas telas progile, create, home

--- CreateAccount/index.js
+++ CreateAccount/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
-import { } from './styles';
+import { StatusBar } from 'react-native';
 
 import { AuthContext } from '../context';
+
+import { Container, Logo, Input, Button, ButtonText } from './styles';
 
 import firebase from 'firebase';
 
@@ -22,49 +23,25 @@ export default CreateAccount = () => {
   const { signUp } = React.useContext(AuthContext)
 
   return (
-    <View style={styles.container}>
-      <View style={styles.view_fields}>
-        <TextInput
-          style={styles.input_auth}
-          onChangeText={text => setTextEmail(text.toLowerCase())}
-          value={textEmail} />
-
-        <TextInput
-          placeholder="Digite sua senha"
-          style={styles.input_auth}
-          onChangeText={text => setTextPassword(text)}
-          value={textPassword} secureTextEntry={true} />
-      </View>
-      <Button title="Criar Conta" onPress={() => handleSignUp()} />
-    </View>
+    <Container>
+    <StatusBar hidden />
+    <Logo source={require('../assets/create-account.png')} resizeMode="contain" />
+      <Input
+        placeholder="Digite seu e-mail"
+        onChangeText={text => setTextEmail(text.toLowerCase())}
+        value={textEmail}
+      />
+      <Input
+        placeholder="Digite sua senha"        
+        onChangeText={text => setTextPassword(text)}
+        value={textPassword} secureTextEntry={true} 
+      />
+    
+    <Button onPress={() => handleSignUp()}>
+      <ButtonText>Criar Conta</ButtonText>
+    </Button>
+  </Container>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginVertical: 10,
-    borderRadius: 5
-  },
-  input_auth: {
-    borderColor: '#ccc',
-    borderWidth: 1,
-    flex: 1,
-    borderRadius: 3,
-    margin: 10,
-    marginTop: 0,
-    padding: 4
-  },
-  view_fields: {
-    flexDirection: 'column',
-    width: '100%',
-    height: 100
-  }
-});
 
